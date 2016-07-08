6cb8a90cc71ab545889a76bc4a0d1aaad1d954e4
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-07-08 21:41:50 +0200

    First skeleton

---
+++ charts_server.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask import Markup
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+import pymongo
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+
+def get_data(topic_name):
+    # Set up client for MongoDB
+    mongoClient=MongoClient()
+    db=mongoClient.SensorData
+    collection=db.home_data
+    yesterday=datetime.today() - timedelta(1)
+    cursor = db.home_data.find({"topic":topic_name,"time":{"$gte":yesterday}})
+
+    values = []
+    labels = []
+
+    for r in cursor:
+        values.append(r['value'])
+        timeStr=r['time'].strftime('%d %b %H:%M')
+        labels.append(timeStr)
+
+    return labels, values
+
+app = Flask(__name__)
+
+    
+@app.route("/")
+def index():
+
+    return render_template('angular.html')
+
+@app.route("/api/Outdoor/Temperature")
+def get_OutdoorTemperature():
+
+    labels, values = get_data("Home/Outdoor/Temperature")
+
+    return jsonify({"measurements":{'labels':labels,'values':values}})
+
+@app.route("/api/GroundFloor/Temperature")
+def get_IndoorTemperature():
+
+    labels, values = get_data("Home/GroundFloor/Temperature")
+
+    return jsonify({"measurements":{'labels':labels,'values':values}})
+
+@app.route("/api/Garage/Temperature")
+def get_GarageTemperature():
+
+    labels, values = get_data("Home/Garage/Temperature")
+
+    return jsonify({"measurements":{'labels':labels,'values':values}})
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=6001,debug=True)
---
+++ static/app.js
@@ -0,0 +1,61 @@
+var myApp = angular.module('app', ["chart.js"])
+    .config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+    });
+
+
+myApp.controller("ChartCtrl", function ($scope,$http) {
+
+var getData = function(url) {
+$http({
+  method: 'GET',
+  url: url
+}).then(function successCallback(response) {
+        var data1 = [response.data.measurements.values];
+        $scope.data.push(data1)
+        $scope.labels.push(response.data.measurements.labels)
+        $scope.titles.push(url)
+
+
+  }, function errorCallback(response) {
+  });
+
+}
+
+$scope.data = []
+$scope.labels = []
+$scope.titles = []
+$scope.series = ['Series A'];
+$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+$scope.options = {
+    scales: {
+        xAxes: [{
+labels: {
+    userCallback: function(dataLabel, index) {
+        return index % 1 === 0 ? dataLabel : '';
+    }
+}
+}],
+    yAxes: [
+        {
+        id: 'y-axis-1',
+        type: 'linear',
+        display: true,
+        position: 'left'
+        },
+        {
+        id: 'y-axis-2',
+        type: 'linear',
+        display: true,
+        position: 'right'
+        }
+    ]
+    }
+};
+
+
+getData('/api/Outdoor/Temperature');
+getData('/api/GroundFloor/Temperature');
+getData('/api/Garage/Temperature');
+
+});
